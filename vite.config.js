53b2a7266eac4416051dcccf6a21280f9cabd174
Lindsey <lindsey@Lindseys-MacBook-Pro.local>
2023-10-19 12:09:30 -0400

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/'
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/'
+  }
+
+  if (command !== 'serve') {
+    configbase = '/portfolio/'
+  }
+
+  return config
+
 })
